954fab5dde10b913e5c4ef7ce4bb049404ed8f9b
antonVosc <anton.vosc@gmail.com>
2025-12-08 22:33:25 +0000

    Show game screen, added cards and can 'turn' them to see the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,75 @@
+import { useEffect, useState } from "react";
+import { Card } from "./components/Card";
+import { GameHeader } from "./components/GameHeader";
+
+const cardValues = [
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🥝",
+  "🥥",
+  "🍉",
+  "🍑",
+  "🍒",
+  "🍎",
+  "🍌",
+  "🍇",
+  "🍊",
+  "🍓",
+  "🥝",
+  "🥥",
+  "🍉",
+  "🍑",
+  "🍒",
+];
+
 function App() {
-  return <>Hello World</>;
+  const [cards, setCards] = useState([]);
+
+  const initializeGame = () => {
+    const finalCards = cardValues.map((value, index) => ({
+      id: index,
+      value,
+      isFlipped: false,
+      isMatched: false,
+    }));
+
+    setCards(finalCards);
+  };
+
+  useEffect(() => {
+    initializeGame();
+  }, []);
+
+  const handleCardClick = (card) => {
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    const newCards = cards.map((c) => {
+      if (c.id === card.id) {
+        return { ...c, isFlipped: true };
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+  };
+
+  return (
+    <div className="app">
+      <GameHeader score={3} moves={10} />
+
+      <div className="cards-grid">
+        {cards.map((card) => (
+          <Card card={card} onClick={handleCardClick} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
